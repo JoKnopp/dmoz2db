ebf6a5faeb71f2341da95c3e505cac9d7dbf8050
Johannes Knopp <johannes@informatik.uni-mannheim.de>
2010-11-29 11:47:32 +0100

    Save all relevant information in the Topic object
    added getters, setters and such

--- src/structure.py
+++ src/structure.py
@@ -49,3 +49,72 @@ class Topic(object):
 
 	def __repr__(self):
 		return '<Topic ' + self.name + '>'
+
+	@property
+	def catid(self):
+		return self.text_vars[DS.CATID]
+
+	@property
+	def title(self):
+		return self.text_vars[DS.TITLE]
+
+	@property
+	def lastupdate(self):
+		return self.text_vars[DS.LASTUPDATE]
+
+	@property
+	def description(self):
+		return self.text_vars[DS.DESCRIPTION]
+
+	def save_attr_by_tag(self, tag, attr):
+		#function calls depending on which attribute is present
+		try:
+			self.attr_by_tag_switch[tag](attr)
+		except KeyError:
+			pass
+
+	def set_altlang(self, attr):
+		"""
+		Adds altlang and resource to self.
+		"""
+		altlang_res = attr.get(DS.resource_attr)
+		altlang, res = altlang_res.split(':')
+		self.altlangs[altlang] = Topic(res)
+
+	def set_letterbar(self, attr):
+		"""
+		Returns a new Topic object from letterbar attribute.
+
+		Also adds a narrow link to self
+		"""
+		res_name = attr.get(DS.resource_attr)
+		new_topic = Topic(res_name)
+		new_topic.letterbar = True
+		#letterbar is a narrow link
+		self.narrow.append(new_topic)
+
+	def append_narrow_link(self, attr):
+		res_name = attr.get(DS.resource_attr)
+		new_topic = Topic(res_name)
+		self.narrow.append(new_topic)
+
+	def set_newsgroup(self, attr):
+		ng_name = attr.get(DS.resource_attr).split(':')[1]
+		self.newsgroups.append(ng_name)
+
+	def set_related(self, attr):
+		res_name = attr.get(DS.resource_attr)
+		new_topic = Topic(res_name)
+		self.add_related_link(new_topic)
+
+	def set_symbolic(self, attr):
+		symboltopic_res = attr.get(DS.resource_attr)
+		topic, res = symboltopic_res.split(':')
+		self.symbolic[topic] = Topic(res)
+
+	def add_related_link(self, topic_obj):
+		assert type(topic_obj) == Topic
+		self.related.append(topic_obj)
+
+	def add_text(self, tag, text):
+		self.text_vars[tag] = text
