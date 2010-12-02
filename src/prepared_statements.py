00b6d9c444e1e6741ddeb31ed012b40c008933a6
Johannes Knopp <johannes@informatik.uni-mannheim.de>
2010-12-02 09:56:28 +0100

    Store Topic data in the database
    Refactored Topic objects. Now they are designed to be used after preparsing
    prepared_statements.py holds the save-to-db sql(alchemy) statements

---
+++ src/prepared_statements.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*
+
+"""
+Object representation for dmoz structure data
+
+Able to store its contents in a database
+"""
+
+from __future__ import unicode_literals
+
+__version__ = '0.1'
+__author__ = 'Johannes Knopp <johannes@informatik.uni-mannheim.de>'
+__copyright__ = '© Copyright 2010 Johannes Knopp'
+
+from sqlalchemy.sql.expression import bindparam
+
+import schemes.table_scheme as ts
+
+ct = ts.categories_t
+rt = ts.related_t
+ngt = ts.newsgroups_t
+alt = ts.altlangs_t
+st = ts.symbolics_t
+
+
+#selects
+sel_by_id = ct.select().where(
+        ct.c.catid==bindparam('cid',required=True))
+sel_by_top = ct.select().where(
+        ct.c.Topic==bindparam('tname',required=True))
+
+#updates
+upd_letterbar = ct.update().where(
+        ct.c.Topic==bindparam('tname',required=True)
+    ).values(letterbar=True)
+upd_lup_desc = ct.update().where(
+        ct.c.catid==bindparam('cid',required=True)
+    ).values(
+        lastupdate=bindparam('lup',required=True)
+    ).values(
+        Description=bindparam('descr',required=True)
+    )
+
+#inserts
+ins_related = rt.insert().values(
+        catid=bindparam('cid',required=True)
+    ).values(
+        rcatid=bindparam('rel_cid',required=True)
+    )
+ins_newsgroup = ngt.insert().values(
+        catid=bindparam('cid',required=True)
+    ).values(
+        newsgroup=bindparam('ngrp',required=True)
+    )
+ins_altlang = alt.insert().values(
+        catid=bindparam('cid',required=True)
+    ).values(
+        language=bindparam('lang',required=True)
+    ).values(
+        resource=bindparam('res',required=True)
+    )
+ins_symbolic = st.insert().values(
+        catid=bindparam('cid',required=True)
+    ).values(
+        scatid=bindparam('scid',required=True)
+    ).values(
+        symbol=bindparam('symb',required=True)
+    )
--- src/structure.py
+++ src/structure.py
@@ -2,6 +2,8 @@
 
 """
 Object representation for dmoz structure data
+
+Able to store its contents in a database
 """
 
 from __future__ import unicode_literals
@@ -10,9 +12,14 @@ __version__ = '0.1'
 __author__ = 'Johannes Knopp <johannes@informatik.uni-mannheim.de>'
 __copyright__ = '© Copyright 2010 Johannes Knopp'
 
-from schemes.xml_scheme import DmozStructure as DS
-
 import sys
+
+from sqlalchemy.sql.expression import bindparam
+from sqlalchemy.exceptions import IntegrityError
+
+from schemes.xml_scheme import DmozStructure as DS
+from prepared_statements import *
+
 
 class Topic(object):
 
@@ -21,18 +28,14 @@ class Topic(object):
 		self.text_vars = {}
 		for tvar in DS.text_tags:
 			self.text_vars[tvar] = ''
-		self.letterbar = False
-		self.narrow = [] #Topic-objects
-		self.related = [] #Topic-objects
+		self.letterbar = [] #Topic-names
+		self.related = [] #Topic-names
 		self.newsgroups = []
-		self.altlangs = {} #lang:Topic-object
-		self.symbolic = {} #topic:Topic-object
+		self.altlangs = {} #Topic-name:lang
+		self.symbolic = {} #Topic-name:symbol
 		self.attr_by_tag_switch = {DS.ALTLANG : self.set_altlang,
 					DS.ALTLANG1 : self.set_altlang,
 					DS.LETTERBAR : self.set_letterbar,
-					DS.NARROW : self.append_narrow_link,
-					DS.NARROW1 : self.append_narrow_link,
-					DS.NARROW2 : self.append_narrow_link,
 					DS.NEWSGROUP : self.set_newsgroup,
 					DS.RELATED : self.set_related,
 					DS.SYMBOLIC : self.set_symbolic,
@@ -66,6 +69,13 @@ class Topic(object):
 		return self.text_vars[DS.DESCRIPTION]
 
 	def save_attr_by_tag(self, tag, attr):
+		"""
+		Calls the store function corresponding to the given tag
+		"""
+		#narrow reverses the isfather relation already handeld by
+		#DmozPreStructureHandler and is therefore ignored
+		if tag in [DS.NARROW, DS.NARROW1, DS.NARROW2]:
+			return
 		#function calls depending on which attribute is present
 		try:
 			self.attr_by_tag_switch[tag](attr)
@@ -78,24 +88,14 @@ class Topic(object):
 		"""
 		altlang_res = attr.get(DS.resource_attr)
 		altlang, res = altlang_res.split(':')
-		self.altlangs[altlang] = Topic(res)
+		self.altlangs[res] = altlang
 
 	def set_letterbar(self, attr):
 		"""
-		Returns a new Topic object from letterbar attribute.
-
-		Also adds a narrow link to self
+		Adds letterbar resources to self.
 		"""
 		res_name = attr.get(DS.resource_attr)
-		new_topic = Topic(res_name)
-		new_topic.letterbar = True
-		#letterbar is a narrow link
-		self.narrow.append(new_topic)
-
-	def append_narrow_link(self, attr):
-		res_name = attr.get(DS.resource_attr)
-		new_topic = Topic(res_name)
-		self.narrow.append(new_topic)
+		self.letterbar.append(res_name)
 
 	def set_newsgroup(self, attr):
 		ng_name = attr.get(DS.resource_attr).split(':')[1]
@@ -103,17 +103,109 @@ class Topic(object):
 
 	def set_related(self, attr):
 		res_name = attr.get(DS.resource_attr)
-		new_topic = Topic(res_name)
-		self.add_related_link(new_topic)
+		self.related.append(res_name)
 
 	def set_symbolic(self, attr):
-		symboltopic_res = attr.get(DS.resource_attr)
-		topic, res = symboltopic_res.split(':')
-		self.symbolic[topic] = Topic(res)
-
-	def add_related_link(self, topic_obj):
-		assert type(topic_obj) == Topic
-		self.related.append(topic_obj)
+		symbol_res = attr.get(DS.resource_attr)
+		symbol, res = symbol_res.split(':')
+		self.symbolic[res] = symbol
 
 	def add_text(self, tag, text):
 		self.text_vars[tag] = text
+
+	def _store_letterbar(self, engine, conn):
+		"""
+		Sets letterbar=True for every self.letterbar entry.
+
+		conn.execute relies on prepared_statements.py
+		"""
+		for topic_name in self.letterbar:
+			conn.execute(upd_letterbar, tname=topic_name)
+
+	def _store_related(self, engine, conn):
+		"""
+		Inserts <catid, relcatid> entries into the related table.
+
+		conn.execute relies on prepared_statements.py
+		"""
+		for rel_top in self.related:
+			rtop_dbdata = conn.execute(sel_by_top, tname=rel_top)
+			relcat = rtop_dbdata.first()
+			if None == relcat:
+				print('relcat of {0} not found: {1}'.format(self.name, rel_top))
+				continue
+
+			relcat_id = relcat[ct.c.catid]
+			try:
+				conn.execute(ins_related, cid=self.catid, rel_cid=relcat_id)
+			except IntegrityError:
+				print('IntegrityError: Entry <catid({0}) relcatid({1})> already exists in table "related"'.format(self.catid, relcat_id))
+
+	def _store_newsgroups(self, engine, conn):
+		"""
+		Inserts <catid, newsgroup> entries into the newsgroups table.
+
+		conn.execute relies on prepared_statements.py
+		"""
+		for ng in self.newsgroups:
+			try:
+				conn.execute(ins_newsgroup, cid=self.catid, ngrp=ng)
+			except IntegrityError:
+				print('IntegrityError: Entry <catid({0}) newsgroup({1})> already exists in table "newsgroups"'.format(self.catid, ng))
+
+	def _store_altlang(self, engine, conn):
+		"""
+		Inserts <catid, language, resource> entries into the altlang table.
+
+		conn.execute relies on prepared_statements.py
+		"""
+		for topic_name, language in self.altlangs.iteritems():
+			try:
+				conn.execute(ins_altlang, cid=self.catid, lang=language, res=topic_name)
+			except IntegrityError:
+				print('IntegrityError: catid({0}) already has an entry for language "{1}"'.format(self.catid, language))
+
+
+	def _store_symbolics(self, engine, conn):
+		"""
+		Inserts <catid, symbol, scatid> entries into the symbolics table.
+
+		conn.execute relies on prepared_statements.py
+		"""
+		for topic_name, symbol in self.symbolic.iteritems():
+			top_dbdata = conn.execute(sel_by_top, tname=topic_name)
+			symbol_cat = top_dbdata.first()
+			if None == symbol_cat:
+				print('symbolic cat of {0} not found: {1}'.format(self.name, topic_name))
+				continue
+
+			symbol_cat_id = symbol_cat[ct.c.catid]
+			try:
+				conn.execute(ins_symbolic, cid=self.catid, symb=symbol, scid=symbol_cat_id)
+			except IntegrityError:
+				print('IntegrityError: catid({0}) already has symbol with reference to refcatid({1})'.format(self.catid, symbol_cat_id))
+
+
+	def _store_lastupdate_and_description(self, engine, conn):
+		"""
+		Updates <lastupdate> and <description> columns in the category table
+
+		conn.execute relies on prepared_statements.py
+		"""
+		conn.execute(upd_lup_desc, cid=self.catid, lup=self.text_vars[DS.LASTUPDATE], descr=self.text_vars[DS.DESCRIPTION])
+
+	def store_in_db(self, engine):
+		"""
+		Stores all attributes in the db.
+
+		Also updates other db entries like the letterbar flag or altlangs
+		conn.execute relies on prepared_statements.py
+		"""
+		conn= engine.connect()
+
+		self._store_letterbar(engine, conn)
+		self._store_related(engine, conn)
+		self._store_newsgroups(engine, conn)
+		self._store_altlang(engine, conn)
+		self._store_symbolics(engine, conn)
+		self._store_lastupdate_and_description(engine, conn)
